c1aa44068c2491ccf0a253e83215784fe7b3128e
aseel <aseelfarrar1@hotmail.com>
2021-08-02 00:46:00 +0300

    init commit

---
+++ src/main/java/com/example/apiPharmacy/ApiPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiPharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPharmacyApplication.class, args);
+	}
+
+}
